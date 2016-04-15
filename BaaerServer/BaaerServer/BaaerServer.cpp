2f399d85297b22e644bbe3fab1692a8cf326a906
miguinarry <A904010@alumni.tecnun.es>
2016-04-15 17:14:59 +0200

    Print timeline
    funcion que impirime el timeline en el server y falta enviar al usuario

--- BaaerServer/BaaerServer/BaaerServer.cpp
+++ BaaerServer/BaaerServer/BaaerServer.cpp
@@ -17,12 +17,6 @@ struct Messages {
   string user_name;
   string content;
   time_t timestamp;
-
-  Messages(const string& a_user_name, const string& a_content) :
-    user_name(a_user_name), content(a_content), id(++last_id)
-  {
-    timestamp = time(0);
-  }
 };
 
 struct Users
@@ -63,27 +57,29 @@ bool EscribirLog(string DATA, string ID){ //return 1: ok
 	
 	return 1;
 }
+//faltaria una funcion para leer dicho archivo
 //funcion para imprimir el timeline cambiar los cout por contenido a enviar y asi enviamos eso
+//global messages[ident].
 int print_timeline(const string& user_name, bool show_id)
 {
   int count = 0;
 
   cout << endl << "----------------" << endl;
   if (user_name.empty()) { // all messages, no filter
-    for (auto it = g_messages.begin(); it != g_messages.end(); ++it) {
+    for (int i=Global_messages.size()-1; i ==0 ; --i) {
       if (show_id) { // print ID of message
-        cout << (*it)->id << " ";
+        cout << Global_messages[i].id << " ";
       }
-      cout << "[@" << (*it)->user_name << "]: " << (*it)->content << endl;
+      cout << "[@" << Global_messages[i].user_name << "]: " << Global_messages[i].content << endl;
     }
-    count = g_messages.size();
+    count = Global_messages.size();
   } else { // specific user
-    for (auto it = g_messages.begin(); it != g_messages.end(); ++it) {
-      if ((*it)->user_name == user_name) {
+    for (int i=Global_messages.size()-1; i ==0 ; --i) {
+      if (Global_messages[i].user_name == user_name) {
         if (show_id) { // print ID of message
-          cout << (*it)->id << " ";
+          cout << Global_messages[i].id << " ";
         }
-        cout << "[@" << user_name << "]: " << (*it)->content << endl;
+        cout << "[@" << user_name << "]: " << Global_messages[i].content << endl;
         ++count;
       }
     }
@@ -132,23 +128,31 @@ int receive(SOCKET ClientSocket){ //return 0 = OK
 				//deserialize:
 				type = deserialize(temporal);
         if(type == "0"){
+			//user and time of conection
 				  user = deserialize(temporal + 5);
 				  cout << "\n\nUsername: " << user;
           LoggedIn.insert (itLogged, pair<string,time_t>(user,time(0)));
         }else if(type == "1"){
+			//new baa
 				  user = deserialize(temporal + 5);
 				  cout << "\n\nUsername: " << user << endl;
           data = deserialize(temporal + 5 + user.length() + 4);
           cout << data;
         }else if(type == "2"){
+			//My baas
         }else if(type == "3"){
-				  user = deserialize(temporal + 5);
-				  cout << "\n\nUsername: " << user;
-          data = deserialize(temporal + 5 + user.length() + 4);
-          cout << data;
+			//Unbaa
+				 
         }else if(type == "4"){
+			//Follow/Unfollow an usser
+			user = deserialize(temporal + 5);
+			cout << "\n\nUsername: " << user;
+            data = deserialize(temporal + 5 + user.length() + 4);
+            cout << data;
         }else if(type == "5"){
+			//Baas timeline
         }else if(type == "6"){
+			//Exit
         }else{
           cout << "Error" << endl;
           // Create error code
@@ -291,9 +295,10 @@ int __cdecl main(void) {
 		}
 		printf("\n\nAccepted!\n");
     /*
-      crear vector de threads, anadir "thread *t = new thread(f)"
+     añadimos un nuevo thread si hay un nuevo client socket,
     */
 		threads.push_back(thread(ThreadFunction, ClientSocket));
+		//el detach es para que los threads puedan correr en paralelo, siendo estos los que hacen la verdadera paralezación de codigo.
 		threads[threads.size()-1].detach();
 	}
 	
